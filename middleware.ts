6f333b13e1811c5b0f311ac6814ebc4c5afc0c7a
Duy Tam <voduytam139@gmail.com>
2023-06-03 21:15:59 +0700

    fix middleware

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,8 @@
-export { default } from 'next-auth/middleware';
+import { withAuth } from 'next-auth/middleware';
+
+export default withAuth({
+  secret: process.env.SECRET,
+});
 
 export const config = {
   matcher: ['/trips', '/reservations', '/properties', '/favorites'],
